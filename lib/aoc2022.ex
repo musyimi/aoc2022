223b66bc886c62750354340e828d7ec658807dee
ngusu <musyimi@users.noreply.github.com>
2022-10-21 12:23:42 +0300

    third puzzle

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -1,3 +1,25 @@
 defmodule Aoc2022 do
+  @callback handle_input(String.t()) :: term
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Aoc2022
+      def example_input() do
+        __ENV__.file
+        |> Path.dirname()
+        |> Path.join("example_input.txt")
+        |> File.read!()
+        |> handle_input()
+      end
+
+      def input() do
+         __ENV__.file
+        |> Path.dirname()
+        |> Path.join("input.txt")
+        |> File.read!()
+        |> handle_input()
+      end
+    end
+  end
 
 end
